0313687d91dfee6c6a66d7aed6ca130a52e20ed6
Carles Muiños <carles.ml.dev@gmail.com>
2013-03-14 13:39:39 +0100

    delete cookbook 'virtualization'

--- cookbooks/virtualization/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'virtualization'
-maintainer       'Carles Muinos'
-maintainer_email 'carles.ml.dev@gmail.com'
-license          'Apache 2.0'
-description      'Installs/Configures virtualization'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
-depends          'apt'
-depends          'base'
