ce72c3138eba5dc297f36c7dc84d2f06b52f3bf9
Carles Muiños <carles.ml.dev@gmail.com>
2014-01-07 16:43:22 +0100

    delete cookbook 'mobile' and refactor accordingly

--- cookbooks/mobile/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'mobile'
-maintainer       'Carles Muinos'
-maintainer_email 'carles.ml.dev@gmail.com'
-license          'Apache 2.0'
-description      'Installs/Configures mobile'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
-depends          'core'
-depends          'base'
