d3600c71db75f020b369f1d3dd5a2142e618b610
Carles Muiños <carles.ml.dev@gmail.com>
2014-01-13 17:09:05 +0100

    release cookbook 'graphics' version 0.1.0

---
+++ cookbooks/graphics/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Author:: Carles Muiños (<carles.ml.dev@gmail.com>)
+# Cookbook Name:: graphics
+# Recipe:: default
+#
+# Copyright 2013,2014 Carles Muiños
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+## Requirements
+
+include_recipe "base"
+
+
+## Deploy
+
+box = node[:box]
+selected = box['apps']['graphics']
+
+if selected
+  graphics = data_bag_item('apps', 'graphics')
+  apps  = graphics['apps']
+
+  # Uninstall apps not needed
+  unselected = apps - selected
+
+  uninstall_apps "graphics" do
+    apps unselected
+    profiles graphics['profiles']
+  end
+
+  # Install selected apps
+  node.set[:apps] = { :graphics => graphics }
+
+  include_recipe "graphics::pinta" if selected.include?("pinta")
+  include_recipe "graphics::shotwell" if selected.include?("shotwell")
+end
+
