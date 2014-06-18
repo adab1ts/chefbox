8425b64f275279f6500fed6d193be864e64255b8
Carles Muiños <carles.ml.dev@gmail.com>
2014-06-18 19:46:47 +0200

    remove cookbook 'desktops' and refactor accordingly

--- cookbooks/desktops/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'desktops'
-maintainer       'Carles Muinos'
-maintainer_email 'carles.ml.dev@gmail.com'
-license          'Apache 2.0'
-description      'Installs/Configures desktops'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
